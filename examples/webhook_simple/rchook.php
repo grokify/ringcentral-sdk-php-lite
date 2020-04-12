782b858e1955ed12faf1d871873b241e4dccd844
John Wang <johncwang@gmail.com>
2020-04-11 22:12:46 -0700

    update quotes

--- examples/webhook_simple/rchook.php
+++ examples/webhook_simple/rchook.php
@@ -2,15 +2,15 @@
 
 date_default_timezone_set('UTC');
 $dt = date('Y-m-d\TH:i:s\Z');
-$logfile = "/var/tmp/rchook.log";
+$logfile = '/var/tmp/rchook.log';
 
-logString($logfile, $dt, "RCHOOK_RECEIVED");
+logString($logfile, $dt, 'RCHOOK_RECEIVED');
 
 if (array_key_exists('HTTP_VALIDATION_TOKEN', $_SERVER)) {
     header("Validation-Token: {$_SERVER['HTTP_VALIDATION_TOKEN']}");
     logString($logfile, $dt, "RCHOOK_HEADER_VALIDATION_TOKEN [{$_SERVER['HTTP_VALIDATION_TOKEN']}]");
 } else {
-    logString($logfile, $dt, "RCHOOK_HEADER_NO_VALIDATION_TOKEN");
+    logString($logfile, $dt, 'RCHOOK_HEADER_NO_VALIDATION_TOKEN');
     $jsonBody = file_get_contents('php://input');
     logString($logfile, $dt, 'RCHOOK_BODY_JSON: ' . $jsonBody);
 }
@@ -19,6 +19,6 @@ function logString($logfile, $dt, $data) {
     error_log($dt . ' ' . $data . "\n", 3, $logfile);
 }
 
-echo "RCHOOK";
+echo 'RCHOOK';
 
 ?>
